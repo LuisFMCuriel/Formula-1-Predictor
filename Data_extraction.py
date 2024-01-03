9d6d0b907ca057a9fa313fac4ce7659ae3163bb1
Luis <felipemoralescuriel@gmail.com>
2024-01-02 21:35:29 -0600

    Data_extraction.py: added data extraction of driver standings

--- Data_extraction.py
+++ Data_extraction.py
@@ -46,3 +46,46 @@ def F1_data_extraction_general(year_start: int = 1950, year_end: int = 2023) -> 
 
     # Convert the list of dictionaries to a DataFrame and return
     return pd.DataFrame(races_list)
+
+def extract_driver_standings(rounds):
+    """
+    Extract driver standings data from the ergast API for the specified rounds.
+
+    Parameters:
+    - rounds (list): List of rounds, where each round is a tuple containing season and a list of round numbers.
+
+    Returns:
+    - pd.DataFrame: DataFrame containing driver standings data.
+    """
+    # Initialize a list to store driver standings data
+    driver_standings_list = []
+
+    # Iterate through the rounds and extract driver standings data
+    for n in range(len(rounds)):
+        for i in rounds[n][1]:
+            # Construct the API URL for driver standings
+            url = f'https://ergast.com/api/f1/{rounds[n][0]}/{i}/driverStandings.json'
+            response = requests.get(url)
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                json_data = response.json()
+
+                # Extract driver standings information from the JSON data
+                standings = json_data.get('MRData', {}).get('StandingsTable', {}).get('StandingsLists', [])[0].get('DriverStandings', [])
+
+                for item in standings:
+                    driver_standings_dict = {
+                        'season': int(json_data['MRData']['StandingsTable']['StandingsLists'][0]['season']),
+                        'round': int(json_data['MRData']['StandingsTable']['StandingsLists'][0]['round']),
+                        'driver': item['Driver'].get('driverId', None),
+                        'driver_points': int(item.get('points', None)),
+                        'driver_wins': int(item.get('wins', None)),
+                        'driver_standings_pos': int(item.get('position', None))
+                    }
+
+                    # Append the driver standings information to the list
+                    driver_standings_list.append(driver_standings_dict)
+
+    # Convert the list of dictionaries to a DataFrame and return
+    return pd.DataFrame(driver_standings_list)
